b96856ae4fda1e03347e7ffdcdd2e75bffb8435a
junho <hwanghe159@naver.com>
2019-12-20 00:35:55 +0900

    cardFactory내부에 카드를 섞는 shuffle()과 한 장 뽑는 pop() 추가

--- src/main/java/domain/card/CardFactory.java
+++ src/main/java/domain/card/CardFactory.java
@@ -8,19 +8,36 @@ import java.util.List;
  * 트럼프 카드 전체 생성을 담당하는 객체
  */
 public class CardFactory {
-    public static List<Card> create() {
-        List<Card> cards = new ArrayList<>();
-        Symbol[] symbols = Symbol.values();
-        for (Symbol symbol : symbols) {
-            createByType(cards, symbol);
-        }
-        return Collections.unmodifiableList(cards);
-    }
+	private static final int TOTAL_NUM_OF_CARDS = 52;
+	List<Card> cardFactory = new ArrayList<Card>();
+	private int index;
 
-    private static void createByType(List<Card> cards, Symbol symbol) {
-        Type[] types = Type.values();
-        for (Type type : types) {
-            cards.add(new Card(symbol, type));
-        }
-    }
+	public CardFactory() {
+		List<Card> cards = new ArrayList<>();
+		Symbol[] symbols = Symbol.values();
+		for (Symbol symbol : symbols) {
+			createByType(cards, symbol);
+		}
+		this.cardFactory = cards;
+		this.index = 0;
+	}
+
+	private static void createByType(List<Card> cards, Symbol symbol) {
+		Type[] types = Type.values();
+		for (Type type : types) {
+			cards.add(new Card(symbol, type));
+		}
+	}
+
+	public void shuffle() {
+		Collections.shuffle(this.cardFactory);
+	}
+
+	public Card pop() {
+		this.index++;
+		if (this.index > TOTAL_NUM_OF_CARDS) {
+			throw new NullPointerException("더 이상 뽑을 카드가 없습니다.");
+		}
+		return cardFactory.get(index);
+	}
 }
--- src/main/java/main/BlackJackGame.java
+++ src/main/java/main/BlackJackGame.java
@@ -3,11 +3,13 @@ package main;
 import java.util.ArrayList;
 import java.util.List;
 
+import domain.card.CardFactory;
 import domain.user.Player;
 
 public class BlackJackGame {
 	
 	List<Player> players = new ArrayList<Player>();
+	CardFactory cardFactory = new CardFactory();
 	
 	public void play() {
 		prepare();
@@ -21,10 +23,15 @@ public class BlackJackGame {
 		for(String playerName : playerNames) {
 			createPlayer(playerName, UI.requestAndInputBettingMoney(playerName));
 		}
+		distributeCards();
 	}
 	
 	public void createPlayer(String name, double bettingMoney) {
 		players.add(new Player(name,bettingMoney));
+	}
+	
+	public void distributeCards() {
+		
 	}
 	
 	public void gamePlay() {
