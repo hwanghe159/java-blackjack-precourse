9ad6cc37183703718177eed94ebd224ce9990839
junho <hwanghe159@naver.com>
2019-12-19 23:46:17 +0900

    플레이어의 이름을 입력받는 inputPlayerNames() 추가

--- src/main/java/main/UI.java
+++ src/main/java/main/UI.java
@@ -1,7 +1,15 @@
 package main;
 
+import java.util.Scanner;
+
 public class UI {
 	public static void requestPlayerNamesMessage() {
 		System.out.println("게임에 참여할 사람의 이름을 입력해주세요.(쉽표 기준으로 분리)");
+	}
+	
+	public static String inputPlayerNames() {
+		Scanner scan = new Scanner(System.in);
+		String playerNames = scan.nextLine();
+		return playerNames;
 	}
 }
