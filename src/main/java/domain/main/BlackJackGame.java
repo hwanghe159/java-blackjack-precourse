7fa75443dffd578e1aba074ff51283f1d0c0082a
junho <hwanghe159@naver.com>
2019-12-19 13:26:40 +0900

    BlackJackGame내부에 play()메서드 생성

--- src/main/java/domain/main/BlackJackGame.java
+++ src/main/java/domain/main/BlackJackGame.java
@@ -1,5 +1,7 @@
 package domain.main;
 
 public class BlackJackGame {
-
+	public void play() {
+		
+	}
 }
--- src/main/java/domain/main/Main.java
+++ src/main/java/domain/main/Main.java
@@ -1,5 +1,8 @@
 package domain.main;
 
 public class Main {
-
+	public static void main(String[] args) {
+		BlackJackGame game = new BlackJackGame();
+		game.play();
+	}
 }
