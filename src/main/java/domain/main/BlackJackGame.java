86692de8fd277a41baeca2aaf7d99a79814e4a2e
junho <hwanghe159@naver.com>
2019-12-19 13:35:58 +0900

    play()를 진행상황에 따라 prepare(), gamePlay(), result()로 분리

--- src/main/java/domain/main/BlackJackGame.java
+++ src/main/java/domain/main/BlackJackGame.java
@@ -2,6 +2,21 @@ package domain.main;
 
 public class BlackJackGame {
 	public void play() {
+		prepare();
+		gamePlay();
+		result();
+	}
+	
+	private void prepare() {
 		
 	}
+	
+	private void gamePlay() {
+		
+	}
+	
+	private void result() {
+		
+	}
+	
 }
