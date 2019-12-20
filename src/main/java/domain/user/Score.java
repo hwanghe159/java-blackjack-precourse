dc826692a5490a0ca73432f8c6b67fdf12bb4cff
junho <hwanghe159@naver.com>
2019-12-20 17:18:14 +0900

    점수를 관리하는 Score클래스 추가

---
+++ src/main/java/domain/user/Score.java
@@ -0,0 +1,18 @@
+package domain.user;
+
+public class Score {
+	private static final int SCORE_MIN = 0;
+	private static final int BLACKJACK_SCORE = 21;
+
+	public static final Score ZERO = new Score(SCORE_MIN);
+
+	private int score;
+
+	public Score(int score) {
+		if (score < SCORE_MIN) {
+			throw new IllegalArgumentException("점수는 0 이상이어야 합니다.");
+		}
+		this.score = score;
+	}
+
+}
